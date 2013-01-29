c0867917a5dd77b248873d7ff1395f506687160b
Dave Clark <me@clarkdave.net>
2013-01-29 23:08:30 +0100

    added js bootstrap stuff

---
+++ public/javascripts/app.js
@@ -0,0 +1,33 @@
+(function(window, document) {
+
+  window.app = window.app || {};
+
+  app.models = {};
+  app.collections = {};
+  app.views = {};
+
+  var MainRouter = Backbone.Router.extend({
+
+    routes: {
+      '': 'home',
+      'search/:query': 'search'
+    },
+
+    home: function() {
+      console.log('home');
+    },
+
+    search: function(query) {
+      console.log('search:', query);
+    }
+  });
+
+  // app initialization
+  
+  app.router = new MainRouter();
+
+  // TODO: fetch models, collections, etc
+  
+  Backbone.history.start({pushState: true});
+
+})(window, document);
